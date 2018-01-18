60a071ff83f78f2a1358dc6b788c2a9600c1d638
Matthew Hu (QA-CN) <matthew_hu@tw.trendnet.org>
2018-01-18 23:51:55 +0800

    revise gitignore

--- proxydriver.go
+++ proxydriver.go
@@ -22,7 +22,7 @@ func main() {
 	prx := new(proxy.Proxy)
 	prx.EnableStatistic()
 	prx.EnableBlackList()
-	prx.SetUpstreamProxy("10.202.241.176:3128")
+	//prx.SetUpstreamProxy("10.64.50.103:4444")
 	prx.EnableAuth()
 	prx.EnablePortMap()
 
